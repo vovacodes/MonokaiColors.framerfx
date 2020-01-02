4bcc731bddd446f8cb41a69037c9665c9409bf42
Vladimir Guguiev <wizardzloy@gmail.com>
2020-01-02 15:01:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Monokai Colors
+
+The package provides the colors from Monokai Pro color theme.
+The colors can be found in the color picker UI under the “Shared Colors” section
+
+### License
+
+All rights belong to Wimer Hazenberg (https://monokai.nl) the original creator of Monokai Pro Theme
---
+++ code/canvas.tsx
@@ -0,0 +1,20 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+
+export const colors = Object.freeze({
+    /** #2C292D */
+    "background": "var(--token-a8397b60-ee40-472a-81d9-988aa827826c, rgb(44, 41, 45))",
+    /** #FF6188 */
+    "red": "var(--token-c161fce1-8009-4fe1-83a3-1eeff8c228f6, rgb(255, 97, 136))",
+    /** #FC9867 */
+    "orange": "var(--token-3cce8099-fbdb-47b0-9109-335dbb571378, rgb(252, 152, 103))",
+    /** #FFD866 */
+    "yellow": "var(--token-08afa0b7-0b05-4191-ba34-b6c90a128bdd, rgb(255, 216, 102))",
+    /** #A9DC76 */
+    "green": "var(--token-2496fada-7043-4040-b90b-9c510cc0cca1, rgb(169, 220, 118))",
+    /** #78DCE8 */
+    "blue": "var(--token-446dc94f-5b9c-4d4b-b78f-0051807cca36, rgb(120, 220, 232))",
+    /** #AB9DF2 */
+    "purple": "var(--token-0d78077a-6d19-46d0-861b-619e34be6e66, rgb(171, 157, 242))",
+})
